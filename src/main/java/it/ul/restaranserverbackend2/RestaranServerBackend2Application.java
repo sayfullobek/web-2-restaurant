f9af3798b38ad3abf506ae38a2b4fe0ac9df5d00
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2023-06-01 04:32:25 -0700

    base

--- src/main/java/it/ul/restaranserverbackend2/RestaranServerBackend2Application.java
+++ src/main/java/it/ul/restaranserverbackend2/RestaranServerBackend2Application.java
@@ -1,13 +1,16 @@
 package it.ul.restaranserverbackend2;
 
+import it.ul.restaranserverbackend2.config.InitConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestaranServerBackend2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestaranServerBackend2Application.class, args);
-	}
+    public static void main(String[] args) {
+        if (InitConfig.isStart()) {
+            SpringApplication.run(RestaranServerBackend2Application.class, args);
+        }
+    }
 
 }
