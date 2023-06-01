25764b0da59d1d39b163007fadba755d368ddbec
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2023-06-01 04:17:16 -0700

    bismillah

---
+++ src/main/java/it/ul/restaranserverbackend2/RestaranServerBackend2Application.java
@@ -0,0 +1,13 @@
+package it.ul.restaranserverbackend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaranServerBackend2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaranServerBackend2Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/ul/restaranserverbackend2/RestaranServerBackend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ul.restaranserverbackend2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaranServerBackend2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
