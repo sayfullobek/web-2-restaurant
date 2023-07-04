e0c2924b8ce99b61aa2c2a7e4da99e2885f7ae67
mukhtarovich-dev <dilbekk3@gmail.com>
2023-07-04 20:40:19 +0300

    nechinchidir

--- src/main/java/it/ul/restaranserverbackend2/entity/SaveProduct.java
+++ src/main/java/it/ul/restaranserverbackend2/entity/SaveProduct.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -18,7 +19,7 @@ public class SaveProduct {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer Id;
     @OneToMany
-    private List<Product> products;
+    private List<Product> products = new ArrayList<>();
 
     public SaveProduct(List<Product> products) {
         this.products = products;
